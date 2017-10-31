f4a8ff783ebd32d0d588c98a53bb3b8365566206
Uwe Riegel <uwriegel@gmail.com>
2017-10-31 12:05:34 +0100

    Successfully displayed icons

--- CommanderWindowsIcons/program.cpp
+++ CommanderWindowsIcons/program.cpp
@@ -6,22 +6,16 @@
 #include "iconextractor.h"
 using namespace std;
 
-#include <Windows.h>
-
 int main(int argc, char* argv[]) {
 	if (argc < 2) 
 		return -1;
 
 	_setmode(_fileno(stdout), _O_BINARY);
 	initialize();
-//	while (true) {
-		//string input;
-		//getline(cin, input);
-		//if (input == "exit")
-		//	break;
-		auto bytes = extract_icon(argv[1]);
-		cout.write(bytes.data(), bytes.size());
-	//}
+
+	auto bytes = extract_icon(argv[1]);
+	cout.write(bytes.data(), bytes.size());
+	cout.flush();
 	uninitialize();
 	return 0;
 }
